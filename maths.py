f6a71d1cd935cb96701a9e9d2c56396ccdb2103f
Fadhl Haqq <punyafadhl@gmail.com>
2023-02-15 23:07:37 +0800

    Update maths.py
    Silly me, JS syntax saved in .py

--- maths.py
+++ maths.py
@@ -1,16 +1,15 @@
 # Interpolate
-def lerp(a, b, t) {
-  return (1 - t) * a + t * b;
-}
+def lerp(a, b, t):
+  return (1 - t) * a + t * b
+
 # Inverse Interpolate
-def inlerp(a, b, v) {
-  return ( v - a ) / ( b - a );
-}
+def inlerp(a, b, v):
+  return ( v - a ) / ( b - a )
+
 # Map
-def map(a, b, c, d, v) {
-    return lerp(c, d, inlerp(a, b, v));
-}
+def map(a, b, c, d, v):
+    return lerp(c, d, inlerp(a, b, v))
+
 # Clamp
-def clamp(a, b, v) {
-    return min(max(a, v), b);
-}
+def clamp(a, b, v):
+    return min(max(a, v), b)
